29e99355d254c3aba3da2d21844e5716e6ae8116
Joyce(Liu QiaoXi) <gogolqx@gmail.com>
2016-07-04 22:47:19 +0200

    Update TestSample.java

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -1,12 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
+// Input Methode Test
 public class TestSample {
 
 
     @Test
-    public void testSampleMethod() {
-
+    public void testFileChooser() throws Exception{
+FileChooser fileChooser= new FileChooser();
+fileChooser.setTitle("Katalog");
+assertEquals("Katalog",fileChooser.evaluate());
+    }
+    
+    @Test
+    public void testDocumentOfClassName() throws Exception{
+Document document = db.parse(new File(excercises.xml));
+String className = document.getClassName("RomanNumberConverter");
+assertEquals("RomanNumberConverter",document.className)
     }
 
 }
